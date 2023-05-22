e9770370baf4ec01d9fe6536e7d7381c3cedd181
jcdentonmj12 <134112720+jcdentonmj12@users.noreply.github.com>
2023-05-21 20:22:47 -0400

    UI changes

--- Assets/TestSceneScripts/PlayerUI/InventoryUI.cs
+++ Assets/TestSceneScripts/PlayerUI/InventoryUI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class InventoryUI : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class InventoryUI : MonoBehaviour
     public GameObject itemUIPrefab; // A prefab for UI item. It should be a Text or TextMeshPro element.
     public GameObject itemUIContainer; // A container for item UI elements. Should be a RectTransform with Vertical Layout Group component.
     public GameObject menuContainer;
+    public Text descriptionText; // New variable, assign the Text component of your new GameObject to it.
+    public ScrollRect scrollRect;
+
+    private int selectedItemIndex = -1;
 
     private void Start()
     {
@@ -21,7 +26,32 @@ public class InventoryUI : MonoBehaviour
         {
             menuContainer.SetActive(!menuContainer.activeSelf);
         }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SelectPreviousItem();
+        }
 
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SelectNextItem();
+        }
+        if (selectedItemIndex >= 0)
+        {
+            scrollRect.verticalNormalizedPosition = 1 - ((float)selectedItemIndex / (inventory.items.Count - 1));
+        }
+
+    }
+
+    private void SelectPreviousItem()
+    {
+        selectedItemIndex = Mathf.Max(selectedItemIndex - 1, 0);
+        UpdateUI();
+    }
+
+    private void SelectNextItem()
+    {
+        selectedItemIndex = Mathf.Min(selectedItemIndex + 1, inventory.items.Count - 1);
+        UpdateUI();
     }
 
     public void UpdateUI()
@@ -33,10 +63,24 @@ public class InventoryUI : MonoBehaviour
         }
 
         // Create new UI items for each item in the inventory
-        foreach (Item item in inventory.items)
+        for (int i = 0; i < inventory.items.Count; i++)
         {
+            Item item = inventory.items[i];
             GameObject itemUIObject = Instantiate(itemUIPrefab, itemUIContainer.transform);
-            itemUIObject.GetComponent<Text>().text = item.name;
+            Text textComponent = itemUIObject.GetComponent<Text>();
+            textComponent.text = item.name;
+
+            // If this is the selected item, highlight it
+            if (i == selectedItemIndex)
+            {
+                textComponent.color = Color.yellow; // Set the color to yellow
+                descriptionText.text = item.description; // Display the description of the selected item.
+            }
+            else
+            {
+                textComponent.color = Color.white; // Set the color to white
+            }
+
             itemUIObject.AddComponent<EventTrigger>();
 
             // Add mouse event
