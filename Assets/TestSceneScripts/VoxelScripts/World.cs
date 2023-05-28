c7f7d29734b87732ff6149004f70d2db0ffc87b5
bloopton <134112720+jcdentonmj12@users.noreply.github.com>
2023-05-28 11:41:13 -0400

    Changed tree gen

--- Assets/TestSceneScripts/VoxelScripts/World.cs
+++ Assets/TestSceneScripts/VoxelScripts/World.cs
@@ -132,7 +132,7 @@ public class World : MonoBehaviour
                 if (random.NextDouble() < treeProbability && terrainHeight > waterLevel)
                 {
                     Vector3Int treePos = new Vector3Int(x, terrainHeight, z);
-                    GenerateTree(terrain, treePos, 8, 3);
+                    GenerateTree(terrain, treePos);
                 }
             }
         }
@@ -140,46 +140,65 @@ public class World : MonoBehaviour
         return terrain;
     }
 
-    private static void GenerateTree(Substance[,,] voxels, Vector3Int position, int trunkHeight, int crownRadius)
+    private static void GenerateTree(Substance[,,] voxels, Vector3Int position)
     {
+        System.Random random = new System.Random();  // Seed this for deterministic tree placement
+
         int width = voxels.GetLength(0);
         int height = voxels.GetLength(1);
         int depth = voxels.GetLength(2);
 
+        // Introduce randomness in tree dimensions
+        int trunkHeight = random.Next(5, 10); // Trees will have height between 3 and 7
+        int trunkThickness = random.Next(1, 4); // Trunk will have thickness between 1 and 3
+        int crownRadius = random.Next(4, 7); // Crown will have radius between 2 and 4
+
         // Generate trunk
-        for (int y = position.y; y < position.y + trunkHeight; y++)
+        for (int xt = position.x; xt < position.x + trunkThickness; xt++)
         {
-            if (y < height)
-            { // Check bounds
-                voxels[position.x, y, position.z] = Substance.wood;
+            for (int zt = position.z; zt < position.z + trunkThickness; zt++)
+            {
+                for (int y = position.y; y < position.y + trunkHeight; y++)
+                {
+                    if (xt < width && zt < depth && y < height)
+                    { // Check bounds
+                        voxels[xt, y, zt] = Substance.wood;
+                    }
+                }
             }
         }
 
-        // Generate crown
+        // Generate crown with a more varied shape
         Vector3Int crownCenter = position + new Vector3Int(0, trunkHeight, 0);
 
         for (int x = crownCenter.x - crownRadius; x <= crownCenter.x + crownRadius; x++)
         {
-            for (int y = crownCenter.y - crownRadius; y <= crownCenter.y + crownRadius; y++)
+            for (int y = crownCenter.y; y <= crownCenter.y + crownRadius; y++)
             {
                 for (int z = crownCenter.z - crownRadius; z <= crownCenter.z + crownRadius; z++)
                 {
                     Vector3Int voxelPosition = new Vector3Int(x, y, z);
-                    // Check if this voxel position is within the sphere (crown)
-                    if (Vector3Int.Distance(voxelPosition, crownCenter) <= crownRadius
+                    // Generate some random roughness factors
+                    float roughnessX = 0.8f + 0.4f * (float)random.NextDouble();
+                    float roughnessY = 0.8f + 0.4f * (float)random.NextDouble();
+                    float roughnessZ = 0.8f + 0.4f * (float)random.NextDouble();
+
+                    // Check if this voxel position is within the "rough" ellipsoid (crown)
+                    if ((Mathf.Pow(voxelPosition.x - crownCenter.x, 2) / Mathf.Pow(crownRadius * roughnessX, 2)
+                        + Mathf.Pow(voxelPosition.y - crownCenter.y, 2) / Mathf.Pow(crownRadius * roughnessY, 2)
+                        + Mathf.Pow(voxelPosition.z - crownCenter.z, 2) / Mathf.Pow(crownRadius * roughnessZ, 2) <= 1
+                        // Special case for the top of the trunk
+                        || (voxelPosition.x >= position.x && voxelPosition.x < position.x + trunkThickness
+                            && voxelPosition.z >= position.z && voxelPosition.z < position.z + trunkThickness
+                            && voxelPosition.y <= position.y + trunkHeight))
                         && x >= 0 && x < width && y >= 0 && y < height && z >= 0 && z < depth)
                     { // Check bounds
-
-                        if (voxels[x, y, z] != Substance.wood)
-                        {
-                            voxels[x, y, z] = Substance.leaf;
-                        }
+                        voxels[x, y, z] = Substance.leaf;
                     }
                 }
             }
         }
     }
-
 
 
 
